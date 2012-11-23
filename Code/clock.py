5aa7e28ff467369046f5d5ebb818fef28e3fb357
user <user@mint>
2012-11-23 21:59:14 +0100

    clock background

--- Code/clock.py
+++ Code/clock.py
@@ -10,33 +10,47 @@ import math
 
 imgLoc="/media/Daten/Bilder/"
 
-def setPointer(pointerId,alpha,dimensions):
-  sx = math.cos(alpha)
-  rx = math.sin(alpha)
-  ry = -math.sin(alpha)
-  sy = math.cos(alpha)
-  subprocess.call(["convert",imgLoc+"minute.png","-virtual-pixel","Transparent","-affine",str(sx)+","+str(rx)+","+str(ry)+","+str(sy)+",0,0","-transform","/tmp/minute.png"])
-  subprocess.call(["composite","-geometry","+1130+10","/tmp/minute.png",imgLoc+"Bender.png",imgLoc+"bg.png"])
+# description of dimensions array:
+# 0 vertikaler Abstand vom oberen Rand des Ziffernblattes zum Drehpunkt
+# 1 horizontaler Abstand vom linken Rand des Ziffernblattes zum Drehpunkt
+# 2 vertikaler Abstand vom oberen Rand der Zeigergrafik zum Zeigerdrehpunkt
+# 3 horizontaler Abstand vom linken Rand der Zeigergrafik zum Zeigerdrehpunkt
+# 4 Hoehe der Zeigergrafik
+# 5 Breite der Zeigergrafik
+def setPointer(pointer,bg,dest,alpha,dim):
+  sinAlpha = math.sin(alpha)
+  cosAlpha = math.cos(alpha)
+  if (alpha <= 0.5 * math.pi):
+    left = int(round(dim[1]-dim[3]*cosAlpha-(dim[4]-dim[2])*sinAlpha))
+    top  = int(round(dim[0]-dim[2]*cosAlpha-dim[3]*sinAlpha))
+  if (alpha > 0.5 * math.pi and alpha <= math.pi):
+    left = int(round(dim[1]+(dim[5]-dim[3])*cosAlpha-(dim[4]-dim[2])*sinAlpha))
+    top  = int(round(dim[0]+(dim[4]-dim[2])*cosAlpha-dim[3]*sinAlpha))
+  if (alpha > math.pi and alpha <= 1.5 * math.pi):
+    left = int(round(dim[1]+(dim[5]-dim[3])*cosAlpha+dim[2]*sinAlpha))
+    top  = int(round(dim[0]+(dim[4]-dim[2])*cosAlpha+(dim[5]-dim[3])*sinAlpha))
+  if (alpha > 1.5 * math.pi):
+    left = int(round(dim[1]-dim[3]*cosAlpha+dim[2]*sinAlpha))
+    top  = int(round(dim[0]-dim[2]*cosAlpha+(dim[5]-dim[3])*sinAlpha))
+  subprocess.call(["convert",pointer,"-virtual-pixel","Transparent","-affine",str(cosAlpha)+","+str(sinAlpha)+","+str(-sinAlpha)+","+str(cosAlpha)+",0,0","-transform","/tmp/pointer_tmp.png"])
+  subprocess.call(["composite","-geometry","+"+str(left)+"+"+str(top)+"","/tmp/pointer_tmp.png",bg,dest])
   return
 
-alpha=1.25*math.pi
-setPointer("",alpha,"")
+curTime = time.localtime(time.time())
+year    = 0
+month   = 1
+day     = 2
+hour    = 3
+minute  = 4
+weekday = calendar.weekday(curTime[year],curTime[month],curTime[day])
 
+curHour = ((curTime[hour] % 12) + curTime[minute] / 60.0)
 
-sys.exit("Ende")
+setPointer(imgLoc+"minute.png",imgLoc+"clock.png","/tmp/clock_1.png",2 * math.pi * curTime[minute] / 60.0,[65.0,65.0,40.0,7.0,61.0,12.0])
+setPointer(imgLoc+"hour.png","/tmp/clock_1.png","/tmp/clock_2.png",2 * math.pi * curHour / 12.0,[65.0,65.0,30.0,7.0,39.0,12.0])
+subprocess.call(["composite","-geometry","+1000+30","/tmp/clock_2.png",imgLoc+"Bender.png",imgLoc+"bg.png"])
+
+sys.exit("")
 
 while True:
   time.sleep(10)
-
-year      = time.localtime(time.time())[0]
-month     = time.localtime(time.time())[1]
-day       = time.localtime(time.time())[2]
-weekday   = calendar.weekday(year,month,day)
-dayMon    = str(day) + "." + str(month)
-date      = dayMon + "." + str(year)
-output    = ""
-
-
-if (output != ""):
-  subprocess.call(["zenity","--info","--text",output])
-

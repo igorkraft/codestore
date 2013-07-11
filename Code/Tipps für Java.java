87d104c70ae8cdeabf1fca38ca602d5beb455c99
igorkraft <igorkraft@web.de>
2013-07-11 21:30:30 +0200

    Update Tipps für Java.java

--- Code/Tipps für Java.java
+++ Code/Tipps für Java.java
@@ -49,6 +49,7 @@ org.apache.commons.io.FileUtils.writeStringToFile(new File("c:\\result.txt"),org
 '\u00e4' = ä
 '\u00fc' = ü
 HTML-Syntax &#x00df;
+HTML dezimal &#223; = ß
 
 • XPath-Helper
 private static String xPathToString(Node node, String expression)

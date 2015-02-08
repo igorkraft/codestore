b99cbdbbbd845a8ad12873c6b14118dc994b968f
Igor Kraft <igorkraft@web.de>
2015-02-08 21:07:44 +0100

    cryptfile_umount.py finished

--- Code/Python/cryptfile_umount.py
+++ Code/Python/cryptfile_umount.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #coding=UTF-8
 
+import shutil
 import subprocess
 import sys
 import uuid
@@ -9,31 +10,46 @@ import os
 # Aufruf: sudo ./cryptfile_umount.py container_file
 # TODO: Abbrechen, wenn das Script nicht als root ausgeführt wird
 
-def getAssociatedParameters(filePath):
+class Associations:
+	mountPoint = ""
+	mapper = ""
+	loopDevice = ""
+
+def getAssociations(filePath):
 	for fso in os.listdir("/media"):
 		if (fso.find("crypt_loop") != 0): continue;
 		p=subprocess.Popen(["losetup", "--show", "/dev/" + fso.replace("crypt_","")],stdout=subprocess.PIPE,stdin=subprocess.PIPE);
 		currentPath = p.stdout.readline().strip().split("(")[1].rstrip(")");
 		if (currentPath == filePath):
-			mountPoint = "/media/" + fso
-			mapper = "/dev/mapper/" + fso
-			loopDevice = "/dev/" + fso.replace("crypt_","")
-			return [mountPoint, mapper, loopDevice];
+			result = Associations();
+			result.mountPoint = "/media/" + fso
+			result.mapper = "/dev/mapper/" + fso
+			result.loopDevice = "/dev/" + fso.replace("crypt_","")
+			return result;
 	return;
 
 if (len(sys.argv) < 2):
 	print "Not enough arguments.";
 	exit();
 
-# TODO: Absoluten Pfad des Dateiparameters ermitteln
-filePath = sys.argv[1];
+# Absoluten Pfad des Dateiparameters ermitteln
+filePath = os.path.realpath(sys.argv[1])
 
 # dessen mountPoint, mapper und loopDevice ermitteln
-params = getAssociatedParameters(filePath);
+associations = getAssociations(filePath);
 
-if (params == None):
+if (associations == None):
 	print "Übergebene Datei ist nicht eingehängt.";
 	exit();
 
-# TODO: die Bindung dieser drei Dinge lösen
-print params;
+# virtuelles Dateisystem aushängen
+subprocess.call(["umount", associations.mountPoint]);
+
+# Mapper auflösen
+subprocess.call(["cryptsetup", "luksClose", associations.mapper]);
+
+# Kontainer aus Loop-Gerät entfernen
+subprocess.call(["losetup", "-d", associations.loopDevice])
+
+# MountPoint entfernen
+shutil.rmtree(associations.mountPoint)

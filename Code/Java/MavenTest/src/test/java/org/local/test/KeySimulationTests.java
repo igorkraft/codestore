d0fb2b73334c5a629e7af8a3d16694276e6584b6
Igor Kraft <igorkraft@web.de>
2015-05-17 16:10:28 +0200

    KeySimulationTests added

---
+++ Code/Java/MavenTest/src/test/java/org/local/test/KeySimulationTests.java
@@ -0,0 +1,36 @@
+package org.local.test;
+
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import org.junit.Test;
+
+public class KeySimulationTests
+{
+	private Robot robot;
+	
+	public KeySimulationTests()
+	{
+		try
+		{
+			this.robot = new Robot();
+		}catch (Exception e) {/* ignore */}
+	}
+	
+	@Test
+	public void keyPressTest() throws Exception
+	{
+		// Simulate a key press
+		this.robot.keyPress(KeyEvent.VK_A);
+		this.robot.keyRelease(KeyEvent.VK_A);
+	}
+	
+//	@Test
+	public void mouseClickTest() throws Exception
+	{
+		// Simulate a mouse click
+		this.robot.mousePress(InputEvent.BUTTON1_MASK);
+		this.robot.mouseRelease(InputEvent.BUTTON1_MASK);
+	}
+}

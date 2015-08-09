2d82f172048ac8f795d054be4099312b16c41ec3
Igor Kraft <igorkraft@web.de>
2015-08-09 23:30:00 +0200

    WebController und Hauptanwendung getrennt

--- Code/Java/SpringBoot/src/main/java/de/at/home/ApplicationMain.java
+++ Code/Java/SpringBoot/src/main/java/de/at/home/ApplicationMain.java
@@ -2,19 +2,12 @@ package de.at.home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 
-@RestController
 @EnableAutoConfiguration // konfiguriert die Anwendung abhängig von ihren Startern
+@ComponentScan
 public class ApplicationMain
 {
-
-	@RequestMapping("/")
-	String root() 
-	{
-		return "Hello World!";
-	}
 
 	public static void main(String[] args) throws Exception 
 	{

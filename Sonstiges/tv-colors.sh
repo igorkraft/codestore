61c10dc83d77f08680b61cf66f62c345b2b9ee82
Igor Kraft <igorkraft@web.de>
2018-04-16 08:58:37 +0200

    tv-colors hinzugefügt

---
+++ Sonstiges/tv-colors.sh
@@ -0,0 +1,13 @@
+# ausführen mit
+# date; while true; do sh Desktop/sound_test.sh; sleep 10; done > /dev/null
+
+# Zeiten in Minuten
+# 17; 5; 20; 4; 1;
+
+speaker-test -c 2 -r 48000 -l 1 -D hw:0,3 ; \
+speaker-test -c 2 -r 48000 -l 1 -D hw:0,3 ; \
+speaker-test -c 2 -r 48000 -l 1 -D hw:0,3 ; \
+speaker-test -c 2 -r 48000 -l 1 -D hw:0,7 ; \
+speaker-test -c 2 -r 48000 -l 1 -D hw:0,8 ; \
+speaker-test -c 2 -r 48000 -l 1 -D hw:0,9 ; \
+speaker-test -c 2 -r 48000 -l 1 -D hw:0,10

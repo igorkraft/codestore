7d149e256c80b1b8426bbbaed01b698c3d8bb036
Igor Kraft <email>
2019-01-11 21:48:14 +0100

    OkHttpClient Tests aktualisiert

--- Code/Java/MavenTest/src/test/java/de/at/home/maventest/OkHttpClientTest.java
+++ Code/Java/MavenTest/src/test/java/de/at/home/maventest/OkHttpClientTest.java
@@ -1,38 +1,35 @@
 package de.at.home.maventest;
 
+import com.google.common.net.HttpHeaders;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.JavaNetCookieJar;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okio.BufferedSink;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
-import java.net.Proxy;
+import java.nio.charset.StandardCharsets;
 import java.security.cert.X509Certificate;
+import java.util.concurrent.TimeUnit;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
-import okio.BufferedSink;
-
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.io.IOUtils;
-import org.junit.Test;
-
-import com.google.common.base.Charsets;
-import com.google.common.net.HttpHeaders;
-import com.squareup.okhttp.Authenticator;
-import com.squareup.okhttp.Headers;
-import com.squareup.okhttp.Interceptor;
-import com.squareup.okhttp.MediaType;
-import com.squareup.okhttp.MultipartBuilder;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
 
 public class OkHttpClientTest
 {
@@ -52,13 +49,13 @@ public class OkHttpClientTest
 	}
 	
 //	@Test
-	public void uploadTest() throws Exception
+	public void uploadTestSynchronous() throws Exception
 	{
 		String url = "http://localhost:8081/upstream/";
 		byte[] file = new byte[]{84, 101, 115, 116, 10, -16, -97, -104, -76, -30, -106, -120, -30, -84, -92, 10, 84, 101, 115, 116, 10};
 		
-		RequestBody requestBody = new MultipartBuilder()
-			.type(MultipartBuilder.FORM)
+		RequestBody requestBody = new MultipartBody.Builder()
+			.setType(MultipartBody.FORM)
 			.addPart(
 					Headers.of(HttpHeaders.CONTENT_DISPOSITION, "form-data; name=\"file\"; filename=\"test.txt\""),
 					RequestBody.create(MediaType.parse("application/octet-stream"), file))
@@ -71,18 +68,18 @@ public class OkHttpClientTest
 	}
 
 //	@Test
-	public void upstreamTest() throws Exception
+	public void upstreamTestAsynchronous() throws Exception
 	{
 		String url = "http://localhost:8081/upstream/";
 		final File file = new File("/path/to/large/file");
-		
-		RequestBody requestBody = new MultipartBuilder()
-			.type(MultipartBuilder.FORM)
+
+		RequestBody requestBody = new MultipartBody.Builder()
+			.setType(MultipartBody.FORM)
 			.addPart(
 					Headers.of(HttpHeaders.CONTENT_DISPOSITION, "form-data; name=\"file\"; filename=\"" + file.getName() + "\""),
 					new RequestBody()
 					{
-						@Override public MediaType contentType() {return MediaType.parse("application/octet-stream");};
+						@Override public MediaType contentType() {return MediaType.parse("application/octet-stream");}
 						
 						@Override
 						public void writeTo(BufferedSink sink) throws IOException
@@ -101,78 +98,60 @@ public class OkHttpClientTest
 						}
 					})
 			.build();
-		
-		Request request = new Request.Builder().url(url).post(requestBody).build();
-		
-		Response response = this.client.newCall(request).execute();
-		System.out.println(response.body().string());
+
+		this.client.newCall(new Request.Builder().url(url).post(requestBody).build()).enqueue(
+			new Callback()
+			{
+				@Override
+				public void onResponse(Call call, Response response) throws IOException
+				{
+					System.out.println(response.body().string());
+				}
+
+				@Override
+				public void onFailure(Call call, IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		);
 	}
-	
+
 	private OkHttpClient createOkHttpClient(final String userName, final String userPassword) throws Exception
 	{
-		OkHttpClient client = new OkHttpClient();
-
-		CookieManager cookieManager = new CookieManager();
-		cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
-		client.setCookieHandler(cookieManager);
-
-//		client.setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("some_proxy", 1234)));
-		
-		client.setHostnameVerifier(new HostnameVerifier()
-		{
-			@Override
-			public boolean verify(String hostname, SSLSession session)
-			{
-				return true;
-			}
-		});
-		
-		client.setAuthenticator(new Authenticator()
-		{
-			@Override
-			public Request authenticate(Proxy proxy, Response response)
-			{
-				String credentials = new String(Base64.encodeBase64((userName + ":" + userPassword).getBytes(Charsets.UTF_8)), Charsets.UTF_8);
-				return response.request().newBuilder().header(HttpHeaders.AUTHORIZATION, "Basic " + credentials).build();
-			}
-			
-			@Override 
-			public Request authenticateProxy(Proxy proxy, Response response) 
-			{
-				return null; // Null indicates no attempt to authenticate.
-			}
-		});
-
-		client.networkInterceptors().add(new Interceptor()
-		{
-			@Override
-			public Response intercept(Chain chain) throws IOException
-			{
-				return chain.proceed(chain.request().newBuilder()
-						.removeHeader(HttpHeaders.USER_AGENT)
-						.addHeader(HttpHeaders.USER_AGENT, "some user agent")
-						.build());
-			}
-		});
-		
 		// Create a trust manager that does not validate certificate chains
-		TrustManager[] trustAllCerts = new TrustManager[] 
+		X509TrustManager trustManager = new X509TrustManager()
 		{
-				new X509TrustManager() 
-				{
-					public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
-					public void checkClientTrusted(X509Certificate[] certs, String authType) {}
-					public void checkServerTrusted(X509Certificate[] certs, String authType) {}
-				}
+			public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+			public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+			public void checkServerTrusted(X509Certificate[] certs, String authType) {}
 		};
 
 		// Install the all-trusting trust manager
 		SSLContext context = SSLContext.getInstance("SSL");
-		context.init(null, trustAllCerts, new java.security.SecureRandom());
-		HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory()); // besser man macht das nur einmal
-		client.setSslSocketFactory(context.getSocketFactory());
+		context.init(null, new TrustManager[]{trustManager}, new java.security.SecureRandom());
+		HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
 
-		return client;
+		return new OkHttpClient.Builder()
+				.connectTimeout(1, TimeUnit.MINUTES)
+				.readTimeout(10, TimeUnit.MINUTES)
+				.writeTimeout(10, TimeUnit.MINUTES)
+				.cookieJar(new JavaNetCookieJar(new CookieManager(){{setCookiePolicy(CookiePolicy.ACCEPT_ALL);}}))
+//				.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("some_proxy", 1234)))
+				.hostnameVerifier((s, sslSession) -> true)
+				.authenticator((route, response) ->
+				{
+					String credentials = new String(Base64.encodeBase64((userName + ":" + userPassword).getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
+					return response.request().newBuilder().header(HttpHeaders.AUTHORIZATION, "Basic " + credentials).build();
+				})
+				.addNetworkInterceptor(chain ->
+						chain.proceed(chain.request().newBuilder()
+								.removeHeader(HttpHeaders.USER_AGENT)
+								.addHeader(HttpHeaders.USER_AGENT, "some user agent")
+								.build())
+				)
+				.sslSocketFactory(context.getSocketFactory(), trustManager)
+				.build();
 	}
 	
 	private class BufferedSinkOutputStream extends OutputStream 
